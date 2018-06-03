ee9e702d284555ee3c59e163ebe0c01f9c34698a
Tim Wanders <timwanders241@gmail.com>
2018-06-03 12:50:02 +0200

    Merge update.sh into install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,7 @@
 source .travis/func.sh
 echo -e "\\n==> Pulling docker image\\n"
 docker pull archlinux/base
+echo -e "\\n==> Updating packages in docker image\\n"
+container=archlinux/base docker_exec "pacman -Syu --noconfirm"
 echo -e "\\n==> Installing dependencies in docker image\\n"
-container=archlinux/base docker_exec pacman -Sy base-devel git shellcheck help2man --noconfirm
+docker_exec pacman -Sy base-devel git shellcheck help2man --noconfirm
--- .travis/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source .travis/func.sh
-echo -e "\\n==> Updating packages\\n"
-docker_exec "pacman -Syu --noconfirm"
