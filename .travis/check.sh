2dcaa1c3496980f4bc5954c665d7a42adecf49a2
Tim Wanders <timwanders241@gmail.com>
2018-06-03 20:42:08 +0200

    Fixed errorcode in travis

--- .travis/check.sh
+++ .travis/check.sh
@@ -2,5 +2,7 @@
 source .travis/func.sh
 echo -e "\\n==> Version information\\n"
 docker_exec "pacman-src --version"
+[ "$exitcode" != "0" ] && exit $exitcode
 echo -e "\\n==> Shellcheck\\n"
-docker_exec "shellcheck -x /usr/bin/pacman-src"
+docker_exec 'shellcheck -x /usr/bin/pacman-src'
+[ "$exitcode" != "0" ] && exit $exitcode
--- .travis/func.sh
+++ .travis/func.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 function docker_exec () {
-	[ ! "$container" ] && container="archlinux/build"
-	docker run $container /bin/sh -c "$*"
+	if [ ! "$container" ]
+	then
+		container="archlinux/build"
+	fi
+	docker run $container /bin/sh -c "$*; exit $?"
+	export exitcode="$?"
 	docker commit $(docker ps -lq | head -1) archlinux/build > /dev/null
 }
